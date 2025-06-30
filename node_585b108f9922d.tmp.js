266746337446d98f2d3f95b8971d0fecb4d51999
Fabia Alessandra Marins Sawaya <ffabia29@hotmail.com>
2025-06-30 14:13:14 -0300

    estrutura de repetição

---
+++ node_585b108f9922d.tmp.js
@@ -0,0 +1,21 @@
+var c = 2
+while (c < 6) {
+    console.log('tudo bem?')
+    c+2  /* c= c + 1 */
+}
+
+
+
+
+
+
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
