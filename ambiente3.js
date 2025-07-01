06b67855a321bebcfcfed83da7ad6f2836f4d020
Fabia Alessandra Marins Sawaya <ffabia29@hotmail.com>
2025-06-30 21:56:51 -0300

    inicio array

---
+++ ambiente3.js
@@ -0,0 +1,9 @@
+let num = [5,8,2,9,3]
+
+num.sort()  /*ordena por tamanho*/
+num.push(5) /*acrescenta*/
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length}posições`)
+console.log(num [3])
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
