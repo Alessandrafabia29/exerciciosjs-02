7917c598f21345deb59515081fd3a0058242e82e
Fabia Alessandra Marins Sawaya <ffabia29@hotmail.com>
2025-06-30 22:25:31 -0300

    atualização

--- ambiente3.js
+++ ambiente3.js
@@ -5,5 +5,12 @@ num.push(5) /*acrescenta*/
 console.log(`Nosso vetor é o ${num}`)
 console.log(`O vetor tem ${num.length}posições`)
 console.log(num [3])
-console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O primeiro valor do vetor é ${num[0]}`) 
+
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(` valor está na posição ${pos}`)
+}
 
